9eb75b044192b68f347e6b42c2cff215235d5bcb
star-code1 <EmailGitHub>
2026-07-03 16:09:15 +0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -7,6 +7,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
   try {
     console.log('⏳ Đang đăng ký slash commands...');
+    console.log("CLIENT_ID:", process.env.CLIENT_ID);
+    console.log("TOKEN:", process.env.DISCORD_TOKEN ? "OK" : "MISSING");
+    console.log("Commands:", commands.length);
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
